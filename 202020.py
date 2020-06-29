318c21ee8820090d26b6d9969edb4ef9e8dedfb5
freddiejbawden <freddiejbawden@gmail.com>
2020-06-29 12:44:57 +0100

    Add v0.1

---
+++ 202020.py
@@ -0,0 +1,67 @@
+import rumps
+
+class ScreentimeApp(object):
+    def __init__(self):
+        self.config = {
+            "app_name": "202020",
+            "start": "Start Timer",
+            "stop": "Stop Timer",
+            "break_message": "Time to rest your eyes, look at a point 20ft away for 20 seconds",
+            "break_done_message": "Time up!",
+            "default_time":"00:00",
+            "interval":  5 # 20*60 # every 20 minutes
+        }
+        self.app = rumps.App(self.config["app_name"])
+        self.countdown_timer = rumps.Timer(self.on_countdown_tick, 1)
+        self.interval = self.config["interval"]
+        self.time_left = 0
+        self.set_up_menu()
+        self.start_button = rumps.MenuItem(title=self.config["start"], callback=self.start_timer)
+        self.stop_button = rumps.MenuItem(title=self.config["stop"], callback=None)
+        self.app.menu = [self.start_button, self.stop_button]
+
+    def set_up_menu(self):
+        self.countdown_timer.stop()
+        self.countdown_timer.count = 0
+        self.app.title = "🖥"
+
+    def on_countdown_tick(self,sender):
+        print(self.time_left)
+        if self.time_left <= 0:
+            rumps.notification(title=self.config["app_name"], subtitle="Eye Rest Reminder", message=self.config["break_message"])
+            self.timer_done()
+            self.stop_button.set_callback(None)
+        else:
+            self.stop_button.set_callback(self.stop_timer)
+            self.time_left -= 1
+  
+
+    def start_timer(self, sender):
+      print('start')
+      rumps.notification(title=self.config["app_name"], subtitle="Timer Started", message='')
+      self.reset_timer()
+
+    def reset_timer(self):
+      self.time_left = self.interval
+      self.countdown_timer.start()
+      self.start_button.set_callback(None)
+
+    def stop_timer(self,sender):
+      self.set_up_menu()
+      self.stop_button.set_callback(None)
+      self.start_button.set_callback(self.start_timer)
+
+
+    def timer_done(self):
+        self.set_up_menu()
+        self.stop_button.set_callback(None)
+        self.start_button.title = self.config["start"]
+        self.reset_timer()
+        
+
+    def run(self):
+        self.app.run()
+
+if __name__ == '__main__':
+    app = ScreentimeApp()
+    app.run()
---
+++ README.md
@@ -0,0 +1,36 @@
+# 20-20-20
+
+Quick OSX app to let you know when to take a quick break from your screen.
+
+The 20-20-20 rule states that every 20 minutes you should look at something 20 feet away for 20 seconds. This helps your eyes relax and reduces strain. The American Academy of Ophthalmolgy recommends this practice to help with strain [article](https://www.aao.org/eye-health/tips-prevention/computer-usage)
+
+Requires Python3!
+
+## Running 
+
+
+### Standalone
+
+The latest standalone is available from the Releases tab
+
+### Python
+```bash
+pip install -r requirements.txt
+python3 202020.py
+```
+
+### Build from source
+
+```
+pip install -r requirements.txt
+pip install py2app
+python setup.py py2app
+```
+# Usage
+
+Once started, a screen icon will appear in your menu bar. Click on the icon and select start timer, this will automatically notify you in 20 minutes. Once the 20 minutes are up, look away from your screen at a point 20ft away for 20 seconds. Afterwards, the timer will start again automatically.
+
+To stop the timer, click the app's icon and select stop timer. Upon restarting the timer will begin from 20 minutes again.
+
+!["Screenshot of the application's menu with start timer selected"](docs/screenshot1.png)
+
